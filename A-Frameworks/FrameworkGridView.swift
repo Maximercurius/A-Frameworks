02f6c4f2dd5b2453ae589abcbadb749a1af8e7f3
Maxim <swiftdevfromsiberia@gmail.com>
2024-03-11 21:26:16 +0500

    1st commit

---
+++ A-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  A-Frameworks
+//
+//  Created by Makarov_Maxim on 11.03.2024.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
