e0b062cf1e5cc36ab77c483764e618af45b0f11d
Maxim <swiftdevfromsiberia@gmail.com>
2024-03-11 11:54:24 +0500

    Initial Commit

---
+++ A-Frameworks/A_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_FrameworksApp.swift
+//  A-Frameworks
+//
+//  Created by Makarov_Maxim on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct A_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
