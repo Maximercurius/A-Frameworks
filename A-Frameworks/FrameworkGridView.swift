99ca4f3488e1577d66f6bad3375a9efdc0637ff5
Maxim <swiftdevfromsiberia@gmail.com>
2024-03-12 22:05:11 +0500

    Grid View added

--- A-Frameworks/FrameworkGridView.swift
+++ A-Frameworks/FrameworkGridView.swift
@@ -13,13 +13,16 @@ struct FrameworkGridView: View {
                               GridItem(.flexible()),
                               GridItem(.flexible())]
     var body: some View {
-        LazyVGrid(columns: colums) {
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: colums) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+
+                    }
+                }
+            }
+            .navigationTitle("🍏 Frameworks")
         }
     }
 }
@@ -28,19 +31,19 @@ struct FrameworkGridView: View {
 }
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
 }
